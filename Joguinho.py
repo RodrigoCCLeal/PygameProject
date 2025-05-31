b2d2d2ce14cc386ecb15e5b1f81723a5200e518c
Rodrigo Leal <rodrigoccleal@poli.ufrj.br>
2025-05-30 22:31:15 -0300

    Colisao

--- Joguinho.py
+++ Joguinho.py
@@ -2,17 +2,21 @@ import pygame
 hero_animation_frame = 0
 hero_start_frame = 0
 hero_pos_x = 1000
-hero_pos_y = 225
+hero_pos_y = 425
 hero_anim_time = 0
 part_animation_frame = 0
 part_start_frame = 0
 part_pos_x = 400
-part_pos_y = 225
+part_pos_y = 425
 part_anim_time = 0
 width = 24 * 60
 height = 24 * 40
-vel = 0.1
+velH = 0.1
+velP = 0.1
 mapa = []
+lColliders = []
+lAguaCol = []
+    
 
 def load_mapa(filename):    #Lê o conteúdo do arquivo para a matriz
     global mapa
@@ -23,7 +27,7 @@ def load_mapa(filename):    #Lê o conteúdo do arquivo para a matriz
 
 def load():
     global clock, playerChar, tileset, tile_wdt, clock, spt_hgt,spt_wdt, partChar,part_wdt,part_hgt
-    global collider_hero, collider_part, collider_paredeL, collider_paredeU, collider_paredeR, collider_paredeD
+    global collider_hero, collider_part
     clock = pygame.time.Clock() 
     load_mapa("mapa.txt")
     tileset = pygame.image.load("tilesetMD.png")
@@ -36,54 +40,79 @@ def load():
     part_wdt = partChar.get_width()/5
     part_hgt = partChar.get_width()/8
 
-    collider_paredeL = pygame.Rect(0, 0, 24, 960)
-    collider_paredeU = pygame.Rect(0, 0, 1440, 24)
-    collider_paredeR = pygame.Rect(1416, 0, 24, 960)
-    collider_paredeD = pygame.Rect(0, 936, 1440, 960)
+
+    for (x,l) in enumerate(mapa):
+        for (y,c) in enumerate(l):
+            if c == "W":
+                parede = pygame.Rect(y*24, x*24, 24, 24)
+                lColliders.append(parede)
+            elif c == "9":
+                parede = pygame.Rect(y*24, x*24, 24, 24)
+                lAguaCol.append(parede)
+        
+
 
 def update(dt):
-    global hero_animation_frame, hero_start_frame, hero_pos_x, hero_pos_y, hero_anim_time, collider_hero
-    global part_animation_frame, part_start_frame, part_pos_y, part_pos_x, part_anim_time, collider_part
+    global hero_animation_frame, hero_start_frame, hero_pos_x, hero_pos_y, hero_anim_time, collider_hero, velH
+    global part_animation_frame, part_start_frame, part_pos_y, part_pos_x, part_anim_time, collider_part, velP
     keys = pygame.key.get_pressed()
     old_hero_x, old_hero_y = hero_pos_x, hero_pos_y
     old_part_x, old_part_y = part_pos_x, part_pos_y
 
+    # Colisão
+    collider_hero = pygame.Rect(hero_pos_x, hero_pos_y, 24, 24)
+    collider_part = pygame.Rect(part_pos_x, part_pos_y, 24, 24)
+
+    if collider_hero.collidelist(lColliders) >= 0:
+        hero_pos_x = old_hero_x
+        hero_pos_y = old_hero_y
+
+    if collider_hero.collidelist(lAguaCol) >= 0:
+        velH = 0.15
+    else:
+       velH = 0.1
+
+        
+    if collider_part.collidelist(lColliders) >= 0:
+        part_pos_x = old_part_x
+        part_pos_y = old_part_y
+
     #Move Mudkip
     if keys[pygame.K_UP] and keys[pygame.K_LEFT]:
         hero_start_frame = 5
-        hero_pos_y = hero_pos_y - (vel * dt)
-        hero_pos_x = hero_pos_x - (vel * dt)
+        hero_pos_y = hero_pos_y - (velH * dt)
+        hero_pos_x = hero_pos_x - (velH * dt)
 
     elif keys[pygame.K_DOWN] and keys[pygame.K_LEFT]:
         hero_start_frame = 7
-        hero_pos_y = hero_pos_y + (vel * dt)
-        hero_pos_x = hero_pos_x - (vel * dt)
+        hero_pos_y = hero_pos_y + (velH * dt)
+        hero_pos_x = hero_pos_x - (velH * dt)
     
     elif keys[pygame.K_UP] and keys[pygame.K_RIGHT]:
         hero_start_frame = 3
-        hero_pos_y = hero_pos_y - (vel * dt)
-        hero_pos_x = hero_pos_x + (vel * dt)
+        hero_pos_y = hero_pos_y - (velH * dt)
+        hero_pos_x = hero_pos_x + (velH * dt)
 
     elif keys[pygame.K_DOWN] and keys[pygame.K_RIGHT]:
         hero_start_frame = 1
-        hero_pos_y = hero_pos_y + (vel * dt)
-        hero_pos_x = hero_pos_x + (vel * dt)
+        hero_pos_y = hero_pos_y + (velH * dt)
+        hero_pos_x = hero_pos_x + (velH * dt)
     
     elif keys[pygame.K_RIGHT]:
         hero_start_frame = 2
-        hero_pos_x = hero_pos_x + (vel * dt)
+        hero_pos_x = hero_pos_x + (velH * dt)
 
     elif keys[pygame.K_LEFT]:
         hero_start_frame = 6
-        hero_pos_x = hero_pos_x - (vel * dt)
+        hero_pos_x = hero_pos_x - (velH * dt)
     
     elif keys[pygame.K_UP]:
         hero_start_frame = 4
-        hero_pos_y = hero_pos_y - (vel * dt)
+        hero_pos_y = hero_pos_y - (velH * dt)
 
     elif keys[pygame.K_DOWN]:
         hero_start_frame = 0
-        hero_pos_y = hero_pos_y + (vel * dt)
+        hero_pos_y = hero_pos_y + (velH * dt)
 
     hero_anim_time = hero_anim_time + dt 
     if hero_anim_time > 100: 
@@ -95,46 +124,46 @@ def update(dt):
     #Move Chimchar
     if keys[pygame.K_w] and keys[pygame.K_a]:
         part_start_frame = 5
-        part_pos_y = part_pos_y - (vel * dt)
-        part_pos_x = part_pos_x - (vel * dt)
+        part_pos_y = part_pos_y - (velP * dt)
+        part_pos_x = part_pos_x - (velP * dt)
          
 
     elif keys[pygame.K_s] and keys[pygame.K_a]:
         part_start_frame = 7
-        part_pos_y = part_pos_y + (vel * dt)
-        part_pos_x = part_pos_x - (vel * dt)
+        part_pos_y = part_pos_y + (velP * dt)
+        part_pos_x = part_pos_x - (velP * dt)
          
     
     elif keys[pygame.K_w] and keys[pygame.K_d]:
         part_start_frame = 3
-        part_pos_y = part_pos_y - (vel * dt)
-        part_pos_x = part_pos_x + (vel * dt)
+        part_pos_y = part_pos_y - (velP * dt)
+        part_pos_x = part_pos_x + (velP * dt)
          
 
     elif keys[pygame.K_s] and keys[pygame.K_d]:
         part_start_frame = 1
-        part_pos_y = part_pos_y + (vel * dt)
-        part_pos_x = part_pos_x + (vel * dt)
+        part_pos_y = part_pos_y + (velP * dt)
+        part_pos_x = part_pos_x + (velP * dt)
          
     
     elif keys[pygame.K_d]:
         part_start_frame = 2
-        part_pos_x = part_pos_x + (vel * dt)
+        part_pos_x = part_pos_x + (velP * dt)
          
 
     elif keys[pygame.K_a]:
         part_start_frame = 6
-        part_pos_x = part_pos_x - (vel * dt)
+        part_pos_x = part_pos_x - (velP * dt)
          
     
     elif keys[pygame.K_w]:
         part_start_frame = 4
-        part_pos_y = part_pos_y - (vel * dt)
+        part_pos_y = part_pos_y - (velP * dt)
          
 
     elif keys[pygame.K_s]:
         part_start_frame = 0
-        part_pos_y = part_pos_y + (vel * dt)
+        part_pos_y = part_pos_y + (velP * dt)
          
     part_anim_time = part_anim_time + dt 
     if part_anim_time > 100: 
@@ -142,36 +171,9 @@ def update(dt):
         if part_animation_frame > 4: 
             part_animation_frame = 0
         part_anim_time = 0
-        
-    
-    
-    collider_hero = pygame.Rect(hero_pos_x, hero_pos_y, 24, 24)
-    if collider_hero.colliderect(collider_paredeL):
-        hero_pos_x = old_hero_x
-        hero_pos_y = old_hero_y
-    if collider_hero.colliderect(collider_paredeU):
-        hero_pos_x = old_hero_x
-        hero_pos_y = old_hero_y
-    if collider_hero.colliderect(collider_paredeR):
-        hero_pos_x = old_hero_x
-        hero_pos_y = old_hero_y
-    if collider_hero.colliderect(collider_paredeD):
-        hero_pos_x = old_hero_x
-        hero_pos_y = old_hero_y
 
-    collider_part = pygame.Rect(part_pos_x, part_pos_y, 24, 24)
-    if collider_part.colliderect(collider_paredeL):
-        part_pos_x = old_part_x
-        part_pos_y = old_part_y
-    if collider_part.colliderect(collider_paredeU):
-        part_pos_x = old_part_x
-        part_pos_y = old_part_y
-    if collider_part.colliderect(collider_paredeR):
-        part_pos_x = old_part_x
-        part_pos_y = old_part_y
-    if collider_part.colliderect(collider_paredeD):
-        part_pos_x = old_part_x
-        part_pos_y = old_part_y
+
+    
 
 
     
@@ -238,3 +240,5 @@ screen = pygame.display.set_mode((width, height))
 load()
 main_loop(screen)
 pygame.quit()
+
+## Debug
